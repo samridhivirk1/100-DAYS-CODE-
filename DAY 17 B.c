a1dcdf6cca4879334012c99047bdf919d956aa4c
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 01:24:37 +0530

    Add prime number checker in DAY 17 B.c

---
+++ DAY 17 B.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, isPrime = 1;
+    scanf("%d", &n);
+
+    if(n <= 1) {
+        isPrime = 0;
+    } else {
+        for(i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if(isPrime) {
+        printf("Prime\n");
+    } else {
+        printf("Not prime\n");
+    }
+
+    return 0;
+}
