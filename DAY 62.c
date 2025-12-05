5f6274cc989e218ebbaca96f2f6dd53415d53b63
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:33:24 +0530

    Create DAY 62.c

---
+++ DAY 62.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max_so_far = arr[0];
+    int max_ending_here = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        max_ending_here = (arr[i] > max_ending_here + arr[i]) ? arr[i] : max_ending_here + arr[i];
+        max_so_far = (max_so_far > max_ending_here) ? max_so_far : max_ending_here;
+    }
+
+    printf("Maximum sum of contiguous subarray is: %d\n", max_so_far);
+    return 0;
+}
