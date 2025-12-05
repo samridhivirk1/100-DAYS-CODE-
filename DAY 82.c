34ebcc4549d990dd188e40f69150fcfb4c0a5f60
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:15:36 +0530

    Create DAY 82.c

---
+++ DAY 82.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+enum TrafficLight { RED, YELLOW, GREEN };
+
+int main() {
+    for (int i = RED; i <= GREEN; i++) {
+        switch (i) {
+            case RED:
+                printf("Traffic Light %d: Stop\n", i);
+                break;
+            case YELLOW:
+                printf("Traffic Light %d: Wait\n", i);
+                break;
+            case GREEN:
+                printf("Traffic Light %d: Go\n", i);
+                break;
+        }
+    }
+    return 0;
+}
