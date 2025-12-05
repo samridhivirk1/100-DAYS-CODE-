2a05f3759c4c7b36551b62bea7381e0a95d00902
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 01:20:12 +0530

    Add program to sum first n odd numbers
    Implement a program to calculate the sum of the first n odd numbers.

--- DAY 14 A.c
+++ DAY 14 A.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int odd(int n);
+int main(){
+    int x;
+    printf("ENTER THE NUMBER OF ODD NUMBERS WHOSE SUM YOU WANT:");
+    scanf("%d",&x);
+    printf("SUM OF FIRST %d ODD NUMBERS IS %d",x,odd(x));
+    return 0;
 
+}
+int odd(int n){
+    int sum = 0;
+    int count = 1;
+
+    for(int i = 1;i<=n;i++,count+=2){
+        
+        sum=sum+count;
+      
+    }
+    return sum;
+}
