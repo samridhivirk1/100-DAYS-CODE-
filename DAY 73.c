1af4e194ce09be22a5e0663ee7b7f7cbaf46d7ca
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:28:29 +0530

    Create DAY 73.c

---
+++ DAY 73.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *file;
+    int ch, prev_char;
+    long char_count = 0, word_count = 0, line_count = 0;
+    int in_word = 0;
+
+    file = fopen("input.txt", "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    while ((ch = fgetc(file)) != EOF) {
+        char_count++;
+
+        if (ch == '\n') {
+            line_count++;
+        }
+
+        if (isspace(ch)) {
+            if (in_word) {
+                in_word = 0;
+                word_count++;
+            }
+        } else {
+            in_word = 1;
+        }
+
+        prev_char = ch;
+    }
+
+    
+    if (in_word) {
+        word_count++;
+    }
+
+    fclose(file);
+
+    printf("Total characters: %ld\n", char_count);
+    printf("Total words: %ld\n", word_count);
+    printf("Total lines: %ld\n", line_count);
+
+    return 0;
+
+}
