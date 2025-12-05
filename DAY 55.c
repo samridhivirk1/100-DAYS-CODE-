4d3ebcca58e410482500a14ff9308368a20f67df
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:45:54 +0530

    Create DAY 55.c

---
+++ DAY 55.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int findMajorityElement(int nums[], int size) {
+    int count = 0;
+    int candidate = -1;
+
+    // Find candidate for majority element
+    for (int i = 0; i < size; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+        }
+        count += (nums[i] == candidate) ? 1 : -1;
+    }
+
+    // Verify if candidate is actually the majority element
+    count = 0;
+    for (int i = 0; i < size; i++) {
+        if (nums[i] == candidate) {
+            count++;
+        }
+    }
+
+    return (count > size / 2) ? candidate : -1;
+}
+
+int main() {
+    int nums[] = {3, 2, 3};
+    int size = sizeof(nums) / sizeof(nums[0]);
+    int majorityElement = findMajorityElement(nums, size);
+
+    if (majorityElement != -1) {
+        printf("Majority element is %d\n", majorityElement);
+    } else {
+        printf("No majority element found\n");
+    }
+
+    return 0;
+}
