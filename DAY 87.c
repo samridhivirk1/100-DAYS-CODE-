07ce1205bf296583b0a8e7b099525c5c74830634
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:24:36 +0530

    Create DAY 87.c

---
+++ DAY 87.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+enum UserRole {
+    ADMIN,
+    USER,
+    GUEST
+};
+
+void displayRoleMessage(enum UserRole role) {
+    switch (role) {
+        case ADMIN:
+            printf("Welcome, Admin! You have full access.\n");
+            break;
+        case USER:
+            printf("Welcome, User! You have limited access.\n");
+            break;
+        case GUEST:
+            printf("Welcome, Guest! Please sign in for more features.\n");
+            break;
+        default:
+            printf("Unknown role.\n");
+    }
+}
+
+int main() {
+    enum UserRole currentRole = USER;
+    displayRoleMessage(currentRole);
+    return 0;
+}
