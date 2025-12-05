af98fc89e51691b4724ca883e3dcf745ed9ccb21
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:22:36 +0530

    Create DAY 83.c

---
+++ DAY 83.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+enum Month { JANUARY = 31, FEBRUARY = 28, MARCH = 31, APRIL = 30, MAY = 31, JUNE = 30, JULY = 31, AUGUST = 31, SEPTEMBER = 30, OCTOBER = 31, NOVEMBER = 30, DECEMBER = 31 };
+
+int main() {
+    for (int i = JANUARY; i <= DECEMBER; i++) {
+        printf("Month %d has %d days\n", i, i);
+    }
+    return 0;
+}
