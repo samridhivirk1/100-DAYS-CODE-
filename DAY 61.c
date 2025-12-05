2ce493577ff7da32caeea2ad1b884c833b5299a5
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:32:55 +0530

    Create DAY 61.c

---
+++ DAY 61.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the size of the subarray: ");
+    scanf("%d", &k);
+
+    for (int i = 0; i <= n - k; i++) {
+        int found = 0;
+        for (int j = i; j < i + k; j++) {
+            if (arr[j] < 0) {
+                printf("%d ", arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            printf("0 ");
+        }
+    }
+    return 0;
+}
