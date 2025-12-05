5442e930c0b121cae20df3f9114dde2932b92468
samridhivirk1 <Samridhi.24003@stu.upes.ac.in>
2025-12-06 02:48:08 +0530

    Create DAY 59.c

---
+++ DAY 59.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the size of the subarray: ");
+    scanf("%d", &k);
+
+    int maxSum = 0;
+    for (int i = 0; i <= n - k; i++) {
+        int currentSum = 0;
+        for (int j = i; j < i + k; j++) {
+            currentSum += arr[j];
+        }
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+        }
+    }
+
+    printf("Maximum sum of subarrays of size %d: %d\n", k, maxSum);
+    return 0;
+}
